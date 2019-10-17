2f9a77a3496278b2904bdfd2f7c04f9dd3671e8c
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-10-17 15:56:47 -0400

    implemented funtionality, and added styles, to display available burgers on the left and devoured burgers on the right.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
 insert into burgers (burger_name) values ('Big Mac');
 insert into burgers (burger_name) values ('Whopper');
 insert into burgers (burger_name) values ('Baconator');
-insert into burgers (burger_name) values ('MacRib');
+insert into burgers (burger_name) values ('McRib');
 insert into burgers (burger_name) values ('Whopper Jr.');
 insert into burgers (burger_name) values ('The Impossible Burger');
 insert into burgers (burger_name) values ('Beyond Burger');
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,1 +1,10 @@
-$()
+const axios = require('')
+$(".devourButtons").on('click', event => {
+  // get the id of the burger being devoured.
+  const id = event.target.attributes[1].value;
+
+  // change the devoured value in the database
+
+
+  // reload the page
+});
