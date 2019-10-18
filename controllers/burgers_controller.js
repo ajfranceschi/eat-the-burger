1c7aa38f9ebc6ca85fa427e32c7374e9a97c0014
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-10-17 23:01:09 -0400

    implemented devouring burgers functionality, deleteAll route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -10,15 +10,57 @@ router.get("/", (req, res) => {
     };
 
     if (!handlebarsObject.burgers) {
-      res.render('error', {error: 'Failed to selectAll burgers.'})
+      res.render("error", { error: "Failed to selectAll burgers." });
     } else {
-      res.render('index', handlebarsObject);
+      res.render("index", handlebarsObject);
     }
-  })
+  });
 });
 
-router.post('/api/insertOne', (req, res) => {
-  res.send('Hello');
+router.post("/api/insertOne", (req, res) => {
+  burger.insertOne(["burger_name"], [req.body.burgerName], response => {
+    res.json({
+      id: response.id
+    });
+  });
+});
+
+router.put("/api/updateOne", (req, res) => {
+  const objectColumnValues = {
+    devoured: req.body.devoured
+  };
+
+  const condition = `id = ${req.body.id}`;
+
+  burger.updateOne(objectColumnValues, condition, response => {
+    if (response.changedRows === 0) {
+      return res.status(404).json("No rows changed.");
+    }
+    res.status(200).json("Updated item");
+  });
+});
+
+router.delete("/api/deleteOne/:id", (req, res) => {
+  const condition = `id=${req.params.id}`;
+  burger.delete(condition, response => {
+    if (response.affectedRows === 0) {
+      return res.status(404).json("Could not find item to be deleted");
+    }
+    res.status(200).json("Burger deleted");
+  });
+});
+
+router.delete("/api/deleteAll", (req, res) => {
+  burger.selectAll(async response => {
+    for (const key in response) {
+      const condition = `id=${response[key].id}`;
+      await burger.delete(condition, () => {
+        console.log(`deleted ${condition}`);
+      });
+      // console.log(await message);
+    }
+  });
+  res.status(200).json("Cleared database");
 });
 
 module.exports = router;
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,10 +1,59 @@
-const axios = require('')
-$(".devourButtons").on('click', event => {
+$(".devourButtons").on("click", event => {
   // get the id of the burger being devoured.
   const id = event.target.attributes[1].value;
 
   // change the devoured value in the database
+  fetch("/api/updateOne", {
+    method: "put",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      id,
+      devoured: true
+    })
+  }).then(response => {
+    if (response.ok) {
+      location.reload();
+    }
+  });
+});
 
+$("#submitBtn").on("click", async () => {
+  const burgerName =$('#burgerTxtField').val().trim();
 
-  // reload the page
+  if (burgerName === '' || !burgerName) {
+    return alert('Please enter a burger before submitting.');
+  }
+
+  const response = fetch('/api/insertOne', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      burgerName
+    })
+  });
+
+  const data = await response;
+
+  if (data.ok) {
+    location.reload();
+  } else {
+    alert('Could not insert item, please try again');
+  }
+});
+
+$('#clearDatabase').on('click', async () => {
+  const response = await fetch('/api/deleteAll', {
+    method: 'delete'
+  });
+  const result = await response;
+  if (result.ok) {
+    location.reload();
+  } else {
+    alert("Couldn't clear database. Please try again later")
+  }
+
 });
